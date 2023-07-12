4bd63e281d2fe12b3ec5cecd714368b4f91cfcd4
IrinaVaronka <varonkairina@gmail.com>
2023-07-12 16:34:02 +0300

    header, footer, main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-function App() {
-  return (
-   <div>Shop</div>
-  );
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Items from "./components/items"
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Safina',
+          img: 'safina.jpg',
+          desc: 'Chair in oak veneer and solid rubber wood',
+          category: 'chairs',
+          price: '49'
+        },
+        {
+          id: 2,
+          title: 'Nina',
+          img: 'Nina.jpg',
+          desc: 'Nina stackable chair in solid acacia wood with walnut finish and beige rope seat',
+          category: 'chairs',
+          price: '80'
+        },
+        {
+          id: 3,
+          title: 'Calixta',
+          img: 'Calixta.jpg',
+          desc: 'Calixta chair in leather and solid teak',
+          category: 'chairs',
+          price: '60'
+        }
+
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+       <Header />
+       <Items items={this.state.items} />
+       <Footer />
+      </div>
+     );
+  }
+
+  
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+   <header>
+        <div>
+            <span className='logo'>House staff</span>
+            <ul className='nav'>
+              <li>About us</li>
+              <li>Contacts</li>
+              <li>Кабинет</li>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+   </header>
+  )
+}
